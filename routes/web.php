25942f243d8471e3ad3ae11b2b055ec3074650b8
Bilala14 <kanianabilamuntaz14@gmail.com>
2025-08-20 02:18:54 +0700

    awal

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ Route::get('/', function () {
     return view('index'); // arahkan ke view index.blade.php
 });
 
+
 Route::post('/biodata', function (Request $request) {
     $pelayanan = $request->pelayanan;
     return view('biodata', compact('pelayanan'));
